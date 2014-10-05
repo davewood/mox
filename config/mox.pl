b744e858df749ce8bdb479ac1fbd88a1ba0cb48d
David Schmidt <mail@davidschmidt.at>
2014-10-05 12:54:37 +0200

    use Dir::Self in config

--- config/mox.pl
+++ config/mox.pl
@@ -1,9 +1,12 @@
 use strict;
 use warnings;
+use Dir::Self;
+
+my $root = __DIR__ . '/../';
 
 return {
     connect_info => {
-        dsn            => 'dbi:SQLite:dbname=mox.db',
+        dsn            => "dbi:SQLite:dbname=$root/mox.db",
         sqlite_unicode => 1,
     },
 };
